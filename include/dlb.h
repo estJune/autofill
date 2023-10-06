b45e9dac6a209eefdbca688f9a0cc030029a82e4
estJune <92344421+estJune@users.noreply.github.com>
2023-10-06 03:15:20 -0400

    added dlb_error nums

--- include/dlb.h
+++ include/dlb.h
@@ -6,7 +6,6 @@
 typedef struct _dlb{
     dlb_node* root;
     unsigned long count;
-    unsigned int curr_key_len;
 } dlb;
 
 #endif
@@ -22,8 +21,13 @@ static void _free_dlb(dlb_node*)
 
 extern void add(dlb*, const char*, unsigned int);
 
-static void _add(dlb_node*, const char*, unsigned int)
-
-
+static void _add(dlb_node*, const char*, unsigned int, unsigned int)
 
 #endif 
+
+#ifndef DLB_ERRORS
+#define DLB_ERRORS
+
+enum dlb_errors {SUCC = 0, ERRNOMEM = 1, ERRINIT}
+
+#endif
