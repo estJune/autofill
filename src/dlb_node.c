e23a8fec1a780c370434d9d148f1593dfcb81cfd
estJune <92344421+estJune@users.noreply.github.com>
2023-10-05 23:36:51 -0400

    created set_letter()

--- src/dlb_node.c
+++ src/dlb_node.c
@@ -1,39 +1,44 @@
 #include <stdlib.h>
 #include "dlb_node.h"
 
-static inline dlb_node* new_dlb_node(char let)
+dlb_node* new_dlb_node()
 {
     dlb_node* new_node = malloc(sizeof(dlb_node));
-    if (!new_node){
-        return NULL
-    }
-    new_node->let = let;
+    if (new_node == NULL)
+        return NULL;
+    new_node->let = ' ';
     new_node->right = NULL;
     new_node->down = NULL;
     return new_node;
 }
 
-static inline void set_right(dlb_node* node, dlb_node* right)
+
+void set_right(dlb_node* node, dlb_node* right)
 {
     node->right = right;
 }
 
-static inline void set_down(dlb_node* node, dlb_node* down)
+void set_down(dlb_node* node, dlb_node* down)
 {
     node->down = down;
 }
 
-static inline dlb_node* get_right(dlb_node* node)
+void set_letter(dlb_node* node, char let)
+{
+    node->let = let;
+}
+
+dlb_node* get_right(dlb_node* node)
 {
     return node->right;
 }
 
-static inline dlb_node* get_down(dlb_node* node)
+dlb_node* get_down(dlb_node* node)
 {
     return node->down;
 }
 
-static inline char get_letter(dlb_node* node)
+char get_letter(dlb_node* node)
 {
     return node->let;
 }
