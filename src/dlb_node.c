e7b36816eb497bbec53f549dea5c26497acb222d
estJune <derrick.daniel.hicks@outlook.com>
2024-02-24 05:54:04 -0500

    removed unecessary functions

--- src/dlb_node.c
+++ src/dlb_node.c
@@ -1,45 +1,14 @@
 #include <stdlib.h>
 #include "dlb_node.h"
 
-dlb_node* new_dlb_node()
+dlb_node* new_dlb_node(char let)
 {
     dlb_node* new_node;
     if (new_node = malloc(sizeof(dlb_node)) == NULL)
         return NULL;
-    new_node->let = '\0';
+    new_node->let = let;
     new_node->freq = 0;
     new_node->right = NULL;
     new_node->down = NULL;
     return new_node;
 }
-
-void set_right(dlb_node* node, dlb_node* right)
-{
-    node->right = right;
-}
-
-void set_down(dlb_node* node, dlb_node* down)
-{
-    node->down = down;
-}
-
-void set_letter(dlb_node* node, char let)
-{
-    node->let = let;
-}
-
-dlb_node* get_right(dlb_node* node)
-{
-    return node->right;
-}
-
-dlb_node* get_down(dlb_node* node)
-{
-    return node->down;
-}
-
-char get_letter(dlb_node* node)
-{
-    return node->let;
-}
-
