1bcc3e0ae1eb5299fdb174a40f732885fe022115
estJune <derrick.daniel.hicks@outlook.com>
2024-02-24 07:03:03 -0500

    feature: added `autofill` files

---
+++ include/autofill.h
@@ -0,0 +1,12 @@
+#include "dlb.h"
+
+
+
+#ifndef AUTOFILL
+#define AUTOFILL
+
+typedef struct _autofill {
+    dlb* dictinoary;                // Dictionary DLB for words that includes words chosen by user and given file
+} autofill;
+
+#endif
